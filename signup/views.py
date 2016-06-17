8baef22f4a7d59f7346cc2602915de565ad63f11
anudeepchen <anudeeprockz@gmail.com>
2016-06-16 18:03:17 -0700

    Changed view and template

--- signup/views.py
+++ signup/views.py
@@ -17,7 +17,7 @@ def register(request):
     context = {"form" : RegisterForm()}
     context.update(csrf(request))
     template = "student_register.html"
-    return render_to_response("student_register.html", {"form" : RegisterForm()},context_instance = RequestContext(request,locals()))
+    return render_to_response("student_register.html",context ,context_instance = RequestContext(request))
 
 
 # Method allows the user to enter the information and submit
@@ -26,9 +26,7 @@ def register_user(request):
         form = RegisterForm(request.POST)
         try :
             if form.is_valid():
-                print "Inside if"
                 email = form.cleaned_data['email']
-                #username = email + 'student'
                 location = form.cleaned_data['location']
                 phone = form.cleaned_data['phone']
                 promo_code = form.cleaned_data['promo_code']
@@ -37,24 +35,18 @@ def register_user(request):
                 new_user.first_name = form.cleaned_data['first_name']
                 new_user.last_name = form.cleaned_data['last_name']
                 new_user.save()
-                
+                    
                 new_form = User_Profile(email=email,phone=phone,promo_code=promo_code,location=location)
                 new_form.save()
-                
-                form = RegisterForm()
                 return HttpResponseRedirect('/')
             else :
-                args['form'] = RegisterForm()
+                print "erroorrrrrr",form.errors
                 messages.error(request, "There is an error in your submission!")
-                template = "student_register.html"
-                return render_to_response(template,args,context_instance = RequestContext(request,locals()))
         except IntegrityError:
-            messages.error(request, "The email you entered is already exists!")
-            form = RegisterForm()
-            template = "student_register.html"
-            return render_to_response(template, context_instance = RequestContext(request,locals()))
+            messages.error(request, "The email you entered already exists!")
     else : form = RegisterForm()
-    template = "registration_complete.html"
-    return render_to_response(template, context_instance = RequestContext(request,locals()))
+    context = {"form" : form}
+    template = "student_register.html"     
+    return render_to_response(template,context, context_instance = RequestContext(request))
 
 
